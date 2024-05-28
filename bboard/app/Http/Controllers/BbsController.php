794deeb8b22102893896a338243c01ae2e5c920c
Pavel M <jelezniydrug@gmail.com>
2024-05-28 12:39:00 -0500

    Model Bb and Filled base with new method in BbsController

--- bboard/app/Http/Controllers/BbsController.php
+++ bboard/app/Http/Controllers/BbsController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Bb;
 class BbsController extends Controller
 {
     public function index()
     {
-        return response('Here will be some advertisements')->header('Content-type', 'text/plain');
+        $bbs = Bb::latest()->get();
+        $s = "Advertisement\r\n\r\n";
+        foreach ($bbs as $bb) {
+            $s .= $bb->title . "\r\n";
+            $s .= $bb->price . " USD.\r\n";
+            $s .=  "\r\n";
+
+        }
+        return response($s)->header('Content-type', 'text/plain');
+    }
+
+    public function detail($bb){
+        $bb = Bb::find($bb);
+        $s = $bb->title . "\r\n";
+        $s .= $bb->price . " USD.\r\n";
+        $s .=  "\r\n";
+        return response($s)->header('Content-type', 'text/plain');
     }
 }
--- bboard/routes/web.php
+++ bboard/routes/web.php
@@ -19,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', [\App\Http\Controllers\BbsController::class, 'index']);
 
+Route::get('/{bb}', [\App\Http\Controllers\BbsController::class, 'detail']);
